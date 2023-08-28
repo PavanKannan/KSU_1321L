70e4930c87454c29e0784b43822f4efdd4973fc9
PavanKannan <143024521+PavanKannan@users.noreply.github.com>
2023-08-28 18:04:33 -0400

    Add files via upload

---
+++ Assignment2A.cpp
@@ -0,0 +1,41 @@
+/*
+Class: CSE 1321L
+Section : E03
+Term : Fall 2023
+Instructor : Illa Jahanshahi
+Name : Pavan Kannan
+Lab# : Assignment2A
+*/
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+  int OctaSide;
+  std::cout << "[OCTAGON]";
+
+  std::cout << "\n\nEnter Length Of One Octagon Side: ";
+  std::cin >> OctaSide;
+
+  double OctaFormula = 2 * (1 + sqrt(2)) * (OctaSide * OctaSide);
+  std::cout << "\n\nOctagon Perimeter: " << OctaSide * 8;
+  std::cout << "\nOctagon Area: " << OctaFormula;
+
+  int CylinderHeight, CylinderRadius;
+  std::cout << "[Cylinder]";
+
+  std::cout << "\n\nEnter Height Of Cylinder: ";
+  std::cin >> CylinderHeight;
+
+  std::cout << "\nEnter Radius Of Cylinder: ";
+  std::cin >> CylinderRadius;
+
+  std::cout << "\n\nCylinder Perimeter: "
+            << 4 * CylinderRadius + 2 * CylinderHeight;
+
+  double CylinderTSA = (2*3.14*CylinderRadius)*(CylinderRadius+CylinderHeight);
+  double CylinderCSA = 2*3.14*CylinderRadius*CylinderHeight;
+  std::cout <<"\nCylinder Total Surface Area: " << CylinderTSA;
+  std::cout <<"\nCylinder Curved Surface Area: " << CylinderCSA;
+  
+}
